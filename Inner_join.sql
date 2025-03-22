185732e8b7c1c0842c5fab043e332eeee779b07f
Priyanshu <priyanshu1272.be21@chitkarauniversity.edu.in>
2025-03-22 09:59:25 +0530

    Added: Inner joins

---
+++ Inner_join.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE University_db;
+USE University_db;
+
+CREATE TABLE Student (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+INSERT INTO Student (student_id,name) VALUES
+(101, 'Rajiv Sharma'),
+(102, 'varun tyagi'),
+(103, 'adarsh verma');
+
+CREATE TABLE Course (
+    student_id INT PRIMARY KEY,
+    course VARCHAR(50)
+);
+
+INSERT INTO Course (student_id,course) VALUES
+(102, 'Java'),
+(105, 'SQL'),
+(103, 'Python'),
+(107,'PowerBi');
+
+SELECT Student.student_id, Student.name, Course.course
+FROM Student
+INNER JOIN Course ON Student.student_id = Course.student_id;
+
+
+
+
+
